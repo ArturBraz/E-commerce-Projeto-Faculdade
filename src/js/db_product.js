d1ab1e47caedbf44a4d571d29208d037cc242146
Artur Braz <documentos.braz@gmail.com>
2022-12-17 01:37:59 -0300

    imagens de produtos,db produtos

---
+++ src/js/db_product.js
@@ -0,0 +1,35 @@
+/* Base de dados dos produtos */
+
+const data = [{
+    id: 1,
+    img: "./src/img/",
+    name: "",
+    price: 0.000,
+    portion: 000,
+    number: [37,38,39,40,41,42,43,44,45,46,47,48],
+    description: "Mais um da coleção Jordan, na coloração original preto e branco com detalhe em dourado perfeito para as quadras.",
+    type: "Basketball",
+    model: "Air Jordan",
+    brand: "Nike",
+    color: "Branco/Preto",
+    category: "men"
+
+},
+{
+    id: 2,
+    img: "./src/img/",
+    name: "",
+    price: 0.000 ,
+    portion: 000,
+    number: [37,38,39,40,41,42,43,44,45,46],
+    description: "Mais um da coleção Jordan, na coloração original preto e branco com detalhe em dourado perfeito para as quadras.",
+    type: "Basketball",
+    model: "Air Jordan",
+    brand: "Nike",
+    color: "Rosa/Cinza",
+    category: "woman"
+},
+{
+
+},
+];
